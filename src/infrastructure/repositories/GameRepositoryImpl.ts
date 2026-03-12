7a2f6fd9f89474bd1174dcf4c125b35373d38ff9
yuuki <bun.yuu88@gmail.com>
2026-03-12 15:40:05 +0900

    N+1問題が発生していたところをPromise.allにして緩和 gitignore修正

--- src/infrastructure/repositories/GameRepositoryImpl.ts
+++ src/infrastructure/repositories/GameRepositoryImpl.ts
@@ -5,23 +5,28 @@ import type { SteamGameDto } from "../../application/interfaces/ISteamService";
 export class GameRepositoryImpl implements IGameRepository {
   constructor(private prisma: PrismaClient) {}
 
-  async syncUserGames(userId: number, games: SteamGameDto[]): Promise<void> {
-    for (const gameDto of games) {
-      // 画像null対策の ?? ""
-      const gameRecord = await this.prisma.game.upsert({
-        where: { steamAppId: gameDto.appId },
-        update: { title: gameDto.name, imageUrl: gameDto.imgIconUrl ?? "" },
-        create: { steamAppId: gameDto.appId, title: gameDto.name, imageUrl: gameDto.imgIconUrl ?? "" },
-      });
+  private readonly DB_QUERY_CHUNK_SIZE = 50;
 
-      //TODO:Promise.allで並列化したい
-      await this.prisma.playStatus.upsert({
-        where: {
-          userId_gameId: { userId: userId, gameId: gameRecord.id },
-        },
-        update: { playtimeForever: gameDto.playtimeForever },
-        create: { userId: userId, gameId: gameRecord.id, playtimeForever: gameDto.playtimeForever },
+  async syncUserGames(userId: number, games: SteamGameDto[]): Promise<void> {
+    for (let i = 0; i < games.length; i += this.DB_QUERY_CHUNK_SIZE) {
+      const chunks = games.slice(i, i + this.DB_QUERY_CHUNK_SIZE);
+
+      const promises = chunks.map(async (gameDto) => {
+        const gameRecord = await this.prisma.game.upsert({
+          where: { steamAppId: gameDto.appId },
+          update: { title: gameDto.name, imageUrl: gameDto.imgIconUrl ?? "" }, //画像null対策の??""
+          create: { steamAppId: gameDto.appId, title: gameDto.name, imageUrl: gameDto.imgIconUrl ?? "" },
+        });
+        //gameのidが必要なので渋々2つに分けている
+        await this.prisma.playStatus.upsert({
+          where: {
+            userId_gameId: { userId: userId, gameId: gameRecord.id },
+          },
+          update: { playtimeForever: gameDto.playtimeForever },
+          create: { userId: userId, gameId: gameRecord.id, playtimeForever: gameDto.playtimeForever },
+        });
       });
+      await Promise.all(promises);
     }
   }
 }
